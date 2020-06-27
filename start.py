5d6e78a21c4591dd8c52d625877c6651040c7011
Aashiq Ramachandran <ramachandran.aashiq@gmail.com>
2020-06-27 10:05:40 +0530

    Update start.py

--- start.py
+++ start.py
@@ -14,9 +14,14 @@ w.make_notebook()
 #initialize the pyshark capture module
 cap =pyshark.LiveCapture(interface='wlan0')
 
+i=0
+
 for packet in cap.sniff_continuously():
 	if 'IP' in packet:
+		#print(packet)
 		#print(packet.ip.field_names)
-		print(packet.ip.flags)
+		print(packet.ip.ttl, i)
+		#print(packet.ip.flags)
 		#print("Source: "+packet.ip.src+" Destination: "+packet.ip.dst)
-		#s.write(packet.ip.src, packet.ip.dst)
+		s.write(int(packet.ip.ttl), i)
+		i=i+1
