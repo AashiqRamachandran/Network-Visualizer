7123c4a362f7382fa54ff73e0d232b93d9819adc
Aashiq Ramachandran <ramachandran.aashiq@gmail.com>
2020-06-27 09:45:25 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,22 @@
+import tensorwatch as tw
+import time
+import pyshark
+
+# streams will be stored in test.log file
+w = tw.Watcher(filename='test.log')
+
+# create a stream for logging
+s = w.create_stream(name='metric1')
+
+# generate Jupyter Notebook to view real-time streams
+w.make_notebook()
+
+#initialize the pyshark capture module
+cap =pyshark.LiveCapture(interface='wlan0')
+
+for packet in cap.sniff_continuously():
+	if 'IP' in packet:
+		#print(packet.ip.field_names)
+		print(packet.ip.flags)
+		#print("Source: "+packet.ip.src+" Destination: "+packet.ip.dst)
+		#s.write(packet.ip.src, packet.ip.dst)
